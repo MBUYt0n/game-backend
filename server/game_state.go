c196181958f3f179f6c4e5d81a940fe5e136d6b8
mbuyt0n <shusrith03@gmail.com>
2026-01-27 22:51:55 +0530

    can run shit

--- server/game_state.go
+++ server/game_state.go
@@ -1,12 +1,13 @@
-package server
+package game_state
+
 import (
 	"game-backend/protos"
 	"math/rand"
 )
 
 type PlayerConn struct {
-	id   string
-	send chan *protos.ServerEvent
+	id    string
+	send  chan *protos.ServerEvent
 	close chan struct{}
 }
 
@@ -14,31 +15,31 @@ type Room struct {
 	players map[string]*PlayerConn
 	state   map[string]*protos.PlayerState
 
-	join   chan *PlayerConn
-	leave  chan *PlayerConn
-	move   chan protos.StateIntentMessage
+	join     chan *PlayerConn
+	leave    chan *PlayerConn
+	move     chan protos.StateIntentMessage
 	snapshot chan bool
 }
 
 func newRoom() *Room {
 	return &Room{
 		players: make(map[string]*PlayerConn),
-		state: make(map[string]*protos.PlayerState),
-		join: make(chan *PlayerConn, 2),
-		leave: make(chan *PlayerConn, 2),
-		move: make(chan protos.StateIntentMessage, 2),
+		state:   make(map[string]*protos.PlayerState),
+		join:    make(chan *PlayerConn, 2),
+		leave:   make(chan *PlayerConn, 2),
+		move:    make(chan protos.StateIntentMessage, 2),
 	}
 }
 
-func (room* Room) run(){
+func (room *Room) run() {
 	var event protos.ServerEvent
 	for {
 		select {
-		case p := <- room.join:
+		case p := <-room.join:
 			event = room.addPlayerToGameState(p)
-		case p := <- room.leave:
+		case p := <-room.leave:
 			event = room.removePlayerFromGameState(p)
-		case m := <- room.move:
+		case m := <-room.move:
 			event = room.movePlayer(&m)
 		}
 		leaveEvents := room.broadcast(&event)
@@ -60,15 +61,15 @@ func getGameState(room *Room) protos.ServerEvent {
 	}
 
 	serverEvent := protos.ServerEvent{
-			Event: &protos.ServerEvent_Snapshot{
-				Snapshot: &snapshot,
-			},
-		}
+		Event: &protos.ServerEvent_Snapshot{
+			Snapshot: &snapshot,
+		},
+	}
 	return serverEvent
-	
+
 }
 
-func (room* Room)broadcast(event *protos.ServerEvent) []protos.ServerEvent{
+func (room *Room) broadcast(event *protos.ServerEvent) []protos.ServerEvent {
 	var leaveEvents []protos.ServerEvent
 	for _, playerConn := range room.players {
 		select {
@@ -83,57 +84,57 @@ func (room* Room)broadcast(event *protos.ServerEvent) []protos.ServerEvent{
 
 }
 
-func (room* Room)addPlayerToGameState(playerConn *PlayerConn) protos.ServerEvent {
+func (room *Room) addPlayerToGameState(playerConn *PlayerConn) protos.ServerEvent {
 	room.players[playerConn.id] = playerConn
 	playerState := protos.PlayerState{
 		PlayerId: playerConn.id,
-		X: int32(rand.Intn(100)),
-		Y: int32(rand.Intn(100)),
+		X:        int32(rand.Intn(100)),
+		Y:        int32(rand.Intn(100)),
 	}
 	room.state[playerConn.id] = &playerState
 	snapshot := getGameState(room)
 	select {
 	case playerConn.send <- &snapshot:
-	default: 
-	joinEvent := protos.ServerGameJoin{
-		PlayerState: &playerState,
-	}
-	serverEvent := protos.ServerEvent{
+	default:
+		joinEvent := protos.ServerGameJoin{
+			PlayerState: &playerState,
+		}
+		serverEvent := protos.ServerEvent{
 			Event: &protos.ServerEvent_GameJoin{
 				GameJoin: &joinEvent,
 			},
 		}
-	return serverEvent
+		return serverEvent
+	}
 }
 
-func (room* Room)removePlayerFromGameState(playerConn *PlayerConn) protos.ServerEvent {
+func (room *Room) removePlayerFromGameState(playerConn *PlayerConn) protos.ServerEvent {
 	delete(room.players, playerConn.id)
 	delete(room.state, playerConn.id)
 	leaveEvent := protos.ServerGameExit{
 		PlayerId: playerConn.id,
 	}
 	serverEvent := protos.ServerEvent{
-			Event: &protos.ServerEvent_GameExit{
-				GameExit: &leaveEvent,
-			},
-		}
+		Event: &protos.ServerEvent_GameExit{
+			GameExit: &leaveEvent,
+		},
+	}
 	return serverEvent
 }
 
-func (room* Room)movePlayer(moveIntent *protos.StateIntentMessage) protos.ServerEvent {
+func (room *Room) movePlayer(moveIntent *protos.StateIntentMessage) protos.ServerEvent {
 	playerState := room.state[moveIntent.PlayerId]
 	playerState.X += moveIntent.Dx
 	playerState.Y += moveIntent.Dy
 	moveEvent := protos.PlayerState{
 		PlayerId: moveIntent.PlayerId,
-		X: playerState.X,
-		Y: playerState.Y,
+		X:        playerState.X,
+		Y:        playerState.Y,
 	}
 	serverEvent := protos.ServerEvent{
-			Event: &protos.ServerEvent_PlayerState{
-				PlayerState: &moveEvent,
-			},
-		}
+		Event: &protos.ServerEvent_PlayerState{
+			PlayerState: &moveEvent,
+		},
+	}
 	return serverEvent
 }
-
--- server/main.go
+++ server/main.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"google.golang.org/grpc"
@@ -9,6 +9,7 @@ import (
 
 )
 
+var room *game_state.Room
 
 type GameServiceServer struct {
 	protos.UnimplementedGameServiceServer
@@ -21,21 +22,65 @@ func main() {
 	grpcServer := grpc.NewServer()
 	protos.RegisterGameServiceServer(grpcServer, &GameServiceServer{})
 	grpcServer.Serve(lis)
+	room = game_state.NewRoom()
+	go room.Run()
+	log.Println("Server is running on port :50051")
 }
 
-func (s* GameServiceServer) Connect(req *protos.ClientEvent, stream grpc.ServerStreamingServer[protos.ServerEvent]) error {
+func (s *GameServiceServer) Connect(
+	stream protos.GameService_ConnectServer,
+) error {
+
+	firstEvent, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+
+	join := firstEvent.GetGameJoin()
+	if join == nil {
+		return status.Error(codes.InvalidArgument, "first event must be GameJoin")
+	}
+
+	playerConn := &game_state.PlayerConn{
+		Id:    join.PlayerId,
+		Send: make(chan *protos.ServerEvent, 16),
+		Close: make(chan struct{}),
+	}
+
+	room.join <- playerConn
+
+	go func() {
+		for {
+			select {
+			case event := <-playerConn.Send:
+				if err := stream.Send(event); err != nil {
+					return
+				}
+			case <-playerConn.Close:
+				return
+			}
+		}
+	}()
+
 	for {
-		event, err := stream.Recv()
-		if err == stream.EOF {
+		clientEvent, err := stream.Recv()
+		if err == io.EOF {
+			room.Leave <- playerConn
 			return nil
 		}
 		if err != nil {
+			room.Leave <- playerConn
 			return err
 		}
 
-		switch event.Event.(type) {
-		case *protos.ClientEvent_GameJoin:
+		switch e := clientEvent.Event.(type) {
 
+		case *protos.ClientEvent_IntentMessage:
+			room.Move <- *e.IntentMessage
+
+		case *protos.ClientEvent_GameExit:
+			room.Leave <- playerConn
+			return nil
 		}
 	}
 }
